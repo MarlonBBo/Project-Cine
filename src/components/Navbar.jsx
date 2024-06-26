4042a0a9ced094ceae7e5791d6b054ec170a73e5
MarlonBBo <mrlonbarretoborges@gmail.com>
2024-06-25 23:24:07 -0300

    incluindo useNavigate no Search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { BiSearchAlt2 } from "react-icons/bi"
 import { MdMovieFilter } from "react-icons/md"
+import { useState } from "react"
 
 import './Navbar.css'
 
 const Navbar = () =>{
+
+  const [Search, SetSearch] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(Search)
+
+    if(!Search) return
+
+    navigate(`/Search?q=${Search}`)
+    SetSearch("")
+  }
+
+
     return (
     <nav id="navbar">
         <h2>
@@ -12,12 +28,21 @@ const Navbar = () =>{
           <MdMovieFilter /> 0NzMovies
           </Link>
         </h2>
-        <form action="">
-          <input type="text" placeholder="Pesquisar filme"/>
+
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            placeholder="Pesquisar filme"
+            onChange={(e) => SetSearch (e.target.value)}
+            value={Search}
+          />
+
           <button type="submit">
             <BiSearchAlt2 />
           </button>
+
         </form>
+
     </nav>
       )
     
