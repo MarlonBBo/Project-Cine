ea7315e1d4afef04b26a53d0367c40ae1393f345
MarlonBBo <mrlonbarretoborges@gmail.com>
2024-07-09 18:02:12 -0300

    Formatação feita!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns"
+
 import { Link } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
@@ -10,7 +12,7 @@ const Card = ({movie, showLink = true}) =>{
         <div className="movie-card">
             <img src={UrlIMG + movie.poster_path} alt={movie.title}/>
             <h2>{movie.title}</h2>
-            <p>{movie.release_date}</p>
+            <p className="format">{format(movie.release_date, "dd/MM/yyyy")}</p>
             <p>
             <FaStar /> {movie.vote_average.toFixed(1)}
             </p>
